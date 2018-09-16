edd6b41f71a6562d743e03f4af4b0b8f180d8f2e
wickedev <orange881217@gmail.com>
2018-09-16 18:22:57 +0900

    v0.0.5

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,38 +1,99 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'
+import { isNotEmpty } from './utils'
+
+const Generator = require('yeoman-generator')
+const chalk = require('chalk')
 
 module.exports = class extends Generator {
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(
-      yosay(`Welcome to the premium ${chalk.red('generator-typescript')} generator!`)
-    );
+    prompting() {
+        this.log(
+            `Welcome to the ${chalk.default.red(
+                '@novonetworks/generator-typescript'
+            )} generator!`
+        )
 
-    const prompts = [
-      {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
-      }
-    ];
+        const prompts = [
+            {
+                type: 'input',
+                name: 'name',
+                message: `question name (${this.appname}): `,
+                default: this.appname
+            },
+            {
+                type: 'input',
+                name: 'version',
+                message: `question version (1.0.0): `,
+                default: '1.0.0'
+            },
+            {
+                type: 'input',
+                name: 'description',
+                message: 'question description: '
+            },
+            {
+                type: 'input',
+                name: 'main',
+                message: 'question entry point (src/index.ts): ',
+                default: 'src/index.ts'
+            },
+            {
+                type: 'input',
+                name: 'url',
+                message: 'question repository url: '
+            },
+            {
+                type: 'input',
+                name: 'author',
+                message: 'question author: '
+            },
+            {
+                type: 'input',
+                name: 'license',
+                message: 'question license (MIT): ',
+                default: 'MIT'
+            },
+            {
+                type: 'input',
+                name: 'private',
+                message: 'question private: '
+            }
+        ]
 
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
-  }
+        return this.prompt(prompts).then(props => {
+            this.props = props
+        })
+    }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
-  }
+    writing() {
+        this.fs.copy(this.templatePath('.'), this.destinationPath('.'))
 
-  install() {
-    this.installDependencies();
-  }
-};
+        const scripts = {
+            start: 'ts-node src/index.ts'
+        }
+
+        this.fs.writeJSON(
+            'package.json',
+            {
+                name: this.props.name,
+                version: this.props.version,
+                description: this.props.description,
+                main: this.props.main,
+                repository: this.props.repository,
+                author: this.props.author,
+                license: this.props.license,
+                private: this.props.private,
+                scripts: scripts
+            },
+            (key, value) => {
+                return value || isNotEmpty(value) ? value : undefined
+            }
+        )
+    }
+
+    async install() {
+        const dependencies = []
+        const devDependencies = ['typescript', 'ts-node']
+        await this.npmInstall(dependencies)
+        await this.npmInstall(devDependencies, { 'save-dev': true })
+    }
+}
---
+++ generators/app/utils.js
@@ -0,0 +1,3 @@
+export function isNotEmpty(message) {
+    return message !== ''
+}
