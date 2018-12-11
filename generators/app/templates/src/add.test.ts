cb432106033302b4b3444cefb709bfbc12233060
wickedev <orange881217@gmail.com>
2018-12-11 12:04:55 +0900

    v1.2.0

--- generators/app/templates/src/add.test.ts
+++
@@ -1,3 +0,0 @@
-it('should one plus one is two', () => {
-    expect(1 + 1).toEqual(2)
-})
---
+++ generators/app/templates/test/add.test.ts
@@ -0,0 +1,5 @@
+import message from '~/sub'
+
+it('should one plus one is two', () => {
+    expect(message).toEqual('Hello Module!')
+})
